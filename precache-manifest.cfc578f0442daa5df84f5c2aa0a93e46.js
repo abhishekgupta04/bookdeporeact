b397ad081eee6586b42adf2560d43ce2308e6859
gupta.abh <gupta.abh>
2020-10-30 15:06:19 +0530

    Updates

---
+++ precache-manifest.cfc578f0442daa5df84f5c2aa0a93e46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af6c0d69a09aac86465bc17618e0296",
+    "url": "/bookdeporeact/index.html"
+  },
+  {
+    "revision": "bd93bd2c9c9923c00b7d",
+    "url": "/bookdeporeact/static/css/main.3fbe023c.chunk.css"
+  },
+  {
+    "revision": "47cfb52f669461c791fa",
+    "url": "/bookdeporeact/static/js/2.4073923c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/bookdeporeact/static/js/2.4073923c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd93bd2c9c9923c00b7d",
+    "url": "/bookdeporeact/static/js/main.c7aa74bf.chunk.js"
+  },
+  {
+    "revision": "3d904bab1d2ad179c3ad",
+    "url": "/bookdeporeact/static/js/runtime-main.7231883a.js"
+  },
+  {
+    "revision": "456bfb5e7008784ad5eea85981844486",
+    "url": "/bookdeporeact/static/media/bg_left.456bfb5e.png"
+  }
+]);
