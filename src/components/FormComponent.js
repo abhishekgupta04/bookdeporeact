95a32e8aa662fc68d02778b29a302d60a2964ddd
gupta.abh <gupta.abh>
2020-11-03 10:22:38 +0530

    changes

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -16,6 +16,7 @@ class FormComponent extends Component {
         localStorage.setItem('bookList', JSON.stringify(items));
         this.props.getData();
         this.printFavCount();
+        this.props.noData();
     }
     //mark fav cards
     markFav = (fav) => {
